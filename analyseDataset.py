c5bae82e4ee2f3e1bb9611e8a9debea719788a56
cdv123 <charles.duboisveltman@gmail.com>
2023-08-04 15:30:04 +0100

    Minor changes

--- analyseDataset.py
+++ analyseDataset.py
@@ -2,7 +2,7 @@ import csv
 # analysis on both training data and test data to make sure both have similar ratio of spam to ham
 spamCountTest = 0
 hamCountTest = 0
-with open("./Dataset/test-data.csv") as file:
+with open("./Dataset/spam_assassin.csv") as file:
     testData = csv.reader(file)
     for row in testData:
         if row[0] == 'ham':
