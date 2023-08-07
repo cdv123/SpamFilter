fd26bfd0bd128463a9fc644497d3129c5b0660de
cdv123 <charles.duboisveltman@gmail.com>
2023-08-07 17:22:04 +0100

    Added necessary import statements

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,9 +1,19 @@
 import numpy as np
-from scipy.special import expit as activation_function
-from scipy.stats import truncnorm
+from sklearn.linear_model import LinearRegression
 import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+from torch.utils.data.dataset import random_split
+from torch.utils.tensorboard import SummaryWriter
+
+import matplotlib.pyplot as plt
 
 #Model
+model = nn.Sequential(
+    nn.Linear(1, 1)
+)
+optim.Adam(model.parameters(), lr = 0.0)
 
-
+our_model = 
 #One-hot encoding
