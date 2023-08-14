5245b43711165fb70bcca2383676da3b48fe1296
cdv123 <charles.duboisveltman@gmail.com>
2023-08-14 21:12:28 +0100

    added model to word2vec file

--- simplifyDataset.py
+++ simplifyDataset.py
@@ -97,3 +97,36 @@ def loadSMS(link):
             else:
                 hamCount+=1
     return (testData,spamList)
+def loadSMS2(link):
+    spamCount = 0
+    hamCount = 0
+    testData = []
+    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~0123456789'''
+    stop_words = set(stopwords.words('english'))
+    spamList = []
+    with open(f"./Dataset/{link}") as file:
+        stop_words = set(stopwords.words('english'))
+        testingCSV= csv.reader(file)
+        for row in testingCSV:
+            text =str(row[0][4:])
+            #Convert to lower case
+            text = text.lower()
+            #Remove stop words
+            row_tokens = word_tokenize(text)
+            new_row = []
+            for token in row_tokens:
+                new_row.append(token)
+            new_row = ' '.join(new_row)
+            for char in new_row:
+                if char in punc:
+                    new_row = new_row.replace(char, "")
+            # new_row = word_stemmer(new_row.split(" "))
+            # new_row= word_lemmatizer(new_row.split(" "))
+            testData.append(new_row)
+            spamList.append(row[0][:3])
+            if spamList[-1] == 'spa':
+                spamCount+=1
+                spamList[-1] = 'spam'
+            else:
+                hamCount+=1
+    return (testData,spamList)
--- word2vec.py
+++ word2vec.py
@@ -15,3 +15,42 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 import gensim
 from gensim.models import word2vec
 #This code used https://datajenius.com/2022/03/13/a-deep-dive-into-nlp-tokenization-encoding-word-embeddings-sentence-embeddings-word2vec-bert/ as a resource, but different application
+
+# Model
+model = nn.Sequential(
+    nn.Linear(300, 1)
+)
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.01)
+my_model = StepByStep(model, loss_fn, optimizer)
+oneHotTrain = torch.as_tensor(oneHotTrain).float()
+spamListTrain = torch.as_tensor(spamListTrain).float()
+oneHotVal = torch.as_tensor(oneHotVal).float()
+spamListVal = torch.as_tensor(spamListVal).float()
+oneHotTest = torch.as_tensor(oneHotTest).float()
+# spamListTest = torch.as_tensor(oneHotTest).float()
+train_dataset = TensorDataset(oneHotTrain,spamListTrain)
+val_dataset = TensorDataset(oneHotVal,spamListVal)
+# test_dataset = TensorDataset(oneHotTest,spamListTest)
+train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(dataset=val_dataset, batch_size=32)
+my_model.set_loaders(train_loader, val_loader)
+my_model.train(100)
+fig = my_model.plot_losses()
+# plt.show()
+my_model.model.eval()
+out = my_model.model(oneHotTest)
+print(out)
+prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
+predictions = []
+for i in prob_sigmoid:
+    if i > 0.5:
+        predictions.append(1)
+    else:
+        predictions.append(0)
+correct = 0
+for i in range(len(predictions)):
+    if predictions[i] == spamListTest[i]:
+        correct+=1
+
+print('accuarcy', correct/len(predictions))
