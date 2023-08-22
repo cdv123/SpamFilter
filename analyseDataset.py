df7d8673ce829c3fac98e3860640167d6fb9a135
cdv123 <charles.duboisveltman@gmail.com>
2023-08-22 12:06:18 +0100

    Combined into one file all the neural network methods

--- analyseDataset.py
+++ analyseDataset.py
@@ -15,6 +15,14 @@ from oneHot import getMostCommonWords
 from gensim import downloader
 from gensim.models import word2vec
 from word2vec import sentenceEmbedding
+
+def createDataset(data,spamList):
+    spamList = np.array(convertSpamToBinary(spamList)).reshape(-1,1)
+    data=np.array([np.array(i) for i in data])
+    data = torch.as_tensor(data).float()
+    spamList = torch.as_tensor(spamList).float()
+    dataset = TensorDataset(data,spamList)
+    return dataset
 def trainNetwork(method_id,trainingData,trainingSpamList,vector_size=300,loss_fn = nn.BCEWithLogitsLoss()):
     model = nn.Sequential(
         nn.Linear(vector_size, 1)
@@ -27,20 +35,27 @@ def trainNetwork(method_id,trainingData,trainingSpamList,vector_size=300,loss_fn
         train_dataset = createDataset(oneHotTrain,trainingSpamList)
     elif method_id == 1:
         word2vec = downloader.load('word2vec-google-news-300')
-
+        trainSentences = sentenceEmbedding(trainingData)
+        train_dataset= createDataset(trainSentences,trainingSpamList)
     elif method_id == 2:
         pass
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
     my_model.set_loaders(train_loader)
     my_model.train(100)
     return my_model
-def useNetwork(model,test):
-    pass
-
-def createDataset(data,spamList):
-    spamList = np.array(convertSpamToBinary(spamList)).reshape(-1,1)
-    data=np.array([np.array(i) for i in data])
-    data = torch.as_tensor(data).float()
-    spamList = torch.as_tensor(spamList).float()
-    dataset = TensorDataset(data,spamList)
-    return dataset
+def useNetwork(model,my_model,testData,testSpamlist):
+    my_model.model.eval()
+    out = my_model.model(testData)
+    prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
+    predictions = []
+    for i in prob_sigmoid:
+        if i > 0.5:
+            predictions.append(1)
+        else:
+            predictions.append(0)
+    correct = 0
+    for i in range(len(predictions)):
+        if predictions[i] == testSpamlist[i]:
+            correct+=1
+    print("accuarcy: ", correct/len(predictions))
+    return predictions
