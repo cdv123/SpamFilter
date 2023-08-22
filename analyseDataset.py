db354203dedd41a92d8839dbf31dbab04aa5d362
cdv123 <charles.duboisveltman@gmail.com>
2023-08-22 18:46:06 +0100

    Fixed file combining all neural network methods

--- analyseDataset.py
+++ analyseDataset.py
@@ -13,17 +13,31 @@ from simplifyDataset import convertSpamToBinary
 from oneHot import oneHotEncode
 from oneHot import getMostCommonWords
 from gensim import downloader
-from gensim.models import word2vec
+from gensim.models import Word2Vec
 from word2vec import sentenceEmbedding
+from customEmbeddings import tokenize
+from customEmbeddings import sentenceEmbedding2
 
-def createDataset(data,spamList):
+def createDataset(data,spamList,valData,valSpamList):
     spamList = np.array(convertSpamToBinary(spamList)).reshape(-1,1)
     data=np.array([np.array(i) for i in data])
     data = torch.as_tensor(data).float()
     spamList = torch.as_tensor(spamList).float()
     dataset = TensorDataset(data,spamList)
-    return dataset
-def trainNetwork(method_id,trainingData,trainingSpamList,vector_size=300,loss_fn = nn.BCEWithLogitsLoss()):
+    valSpamList = np.array(convertSpamToBinary(valSpamList)).reshape(-1,1)
+    valData = np.array([np.array(i) for i in valData])
+    valData = torch.as_tensor(valData).float()
+    valSpamList = torch.as_tensor(valSpamList).float()
+    return dataset,data,spamList,valData,valSpamList
+def toTensor(data,spamList):
+    spamList = np.array(convertSpamToBinary(spamList)).reshape(-1,1)
+    data=np.array([np.array(i) for i in data])
+    data = torch.as_tensor(data).float()
+    spamList = torch.as_tensor(spamList).float()
+    dataset = TensorDataset(data,spamList)
+    return dataset,data,spamList
+def trainNetwork(method_id,trainingData,trainingSpamList,valData,valSpamList,vector_size=500,loss_fn = nn.BCEWithLogitsLoss()):
+    mostCommonWords =[]
     model = nn.Sequential(
         nn.Linear(vector_size, 1)
     )   
@@ -32,20 +46,51 @@ def trainNetwork(method_id,trainingData,trainingSpamList,vector_size=300,loss_fn
     if method_id == 0:
         mostCommonWords = getMostCommonWords(trainingData,vector_size)
         oneHotTrain = oneHotEncode(trainingData,mostCommonWords).values()
-        train_dataset = createDataset(oneHotTrain,trainingSpamList)
+        oneHotVal = oneHotEncode(valData,mostCommonWords).values()
+        train_dataset,oneHotTrain,trainingSpamList,oneHotVal,valSpamList = createDataset(oneHotTrain,trainingSpamList,oneHotVal,valSpamList)
+        val_dataset = TensorDataset(oneHotVal,valSpamList)
     elif method_id == 1:
         word2vec = downloader.load('word2vec-google-news-300')
-        trainSentences = sentenceEmbedding(trainingData)
-        train_dataset= createDataset(trainSentences,trainingSpamList)
+        mostCommonWords = word2vec
+        trainSentences = sentenceEmbedding(trainingData,trainingSpamList,word2vec,vector_size)
+        valSentences = sentenceEmbedding(valData,valSpamList,word2vec,vector_size)
+        train_dataset,trainSentences,trainingSpamList,valSentences,valSpamList= createDataset(trainSentences,trainingSpamList,valSentences,valSpamList)
+        val_dataset = TensorDataset(valSentences,valSpamList)
     elif method_id == 2:
-        pass
+        wordEmbedding = tokenize(trainingData)
+        word2vec = Word2Vec(wordEmbedding,vector_size=vector_size,min_count=1,workers=4,window=5,sg=1)
+        mostCommonWords = word2vec
+        trainSentences = np.array(sentenceEmbedding2(trainingData,trainingSpamList,word2vec,vector_size))
+        trainingSpamList = np.array(convertSpamToBinary(trainingSpamList)).reshape(-1,1)
+        valSentences = np.array(sentenceEmbedding2(valData,valSpamList,word2vec,vector_size))
+        valSpamList = np.array(convertSpamToBinary(valSpamList)).reshape(-1,1)
+        trainSentences = torch.as_tensor(trainSentences).float()
+        trainingSpamList = torch.as_tensor(trainingSpamList).float()
+        valSentences = torch.as_tensor(valSentences).float()
+        valSpamList = torch.as_tensor(valSpamList).float()
+        train_dataset = TensorDataset(trainSentences,trainingSpamList)
+        val_dataset = TensorDataset(valSentences,valSpamList)
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-    my_model.set_loaders(train_loader)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=32)
+    my_model.set_loaders(train_loader,val_loader)
     my_model.train(100)
-    return my_model
-def useNetwork(model,my_model,testData,testSpamlist):
+    return my_model,mostCommonWords
+def useNetwork(my_model,testData,testSpamList,method_id,vector_size,mostCommonWords):
     my_model.model.eval()
-    out = my_model.model(testData)
+    if method_id == 0:
+        oneHotTest = oneHotEncode(testData,mostCommonWords).values()
+        test_dataset,oneHotTest,testSpamList = toTensor(oneHotTest,testSpamList)
+        out = my_model.model(oneHotTest)
+    elif method_id == 1:
+        testSentences = sentenceEmbedding(testData,testSpamList,mostCommonWords,vector_size)
+        test_dataset,testSentences,testSpamList = toTensor(testSentences,testSpamList)
+        out = my_model.model(testSentences)
+    elif method_id == 2:
+        testSentences = sentenceEmbedding2(testData,testSpamList,mostCommonWords,vector_size)
+        testSpamList = convertSpamToBinary(testSpamList)
+        testSentences = np.array([np.array(i) for i in testSentences])
+        testSentences = torch.as_tensor(testSentences).float()
+        out = my_model.model(testSentences)
     prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
     predictions = []
     for i in prob_sigmoid:
@@ -55,7 +100,12 @@ def useNetwork(model,my_model,testData,testSpamlist):
             predictions.append(0)
     correct = 0
     for i in range(len(predictions)):
-        if predictions[i] == testSpamlist[i]:
+        if predictions[i] == testSpamList[i]:
             correct+=1
-    print("accuarcy: ", correct/len(predictions))
+    print("accuarcy:", correct/len(predictions))
     return predictions
+trainingData,trainingSpamList = loadSMS("SMSSpamCollection.csv")
+valData,valSpamList = loadSMS("SMSVal.csv")
+testData,testSpamList = loadSMS("SMSTest.csv")
+my_model,word2vec = trainNetwork(2,trainingData,trainingSpamList,valData,valSpamList)
+useNetwork(my_model,testData,testSpamList,2,500,word2vec)
--- word2vec.py
+++ word2vec.py
@@ -16,19 +16,19 @@ from gensim import downloader
 
 #Download vocab
 
-word2vec = downloader.load('word2vec-google-news-300')
+# word2vec = downloader.load('word2vec-google-news-300')
 
-trainingData, trainingSpamList = loadSMS('SMSSpamCollection.csv')
-valData, valSpamList = loadSMS('SMSVal.csv')
-testData, testSpamList = loadSMS('SMSTest.csv')
+# trainingData, trainingSpamList = loadSMS('SMSSpamCollection.csv')
+# valData, valSpamList = loadSMS('SMSVal.csv')
+# testData, testSpamList = loadSMS('SMSTest.csv')
 
 #average out all word embeddings into sentence embedding
-def sentenceEmbedding(data,spamList):
+def sentenceEmbedding(data,spamList,word2vec,vector_size):
     sentenceEmbeddings = []
     dataLength = len(data)
     i = 0
     while i < dataLength:
-        sentence = np.zeros(300)
+        sentence = np.zeros(vector_size)
         row = data[i].split(" ")
         length = 0
         for j in row:
@@ -50,51 +50,51 @@ def sentenceEmbedding(data,spamList):
         i+=1
     return sentenceEmbeddings
 
-trainSentences = sentenceEmbedding(trainingData,trainingSpamList)
-trainingSpamList = np.array(convertSpamToBinary(trainingSpamList))
-trainingSpamList = trainingSpamList.reshape(-1,1)
-trainSentences = np.array(trainSentences)
-valSentences = sentenceEmbedding(valData,valSpamList)
-valSpamList = np.array(convertSpamToBinary(valSpamList))
-valSpamList = valSpamList.reshape(-1,1)
-valSentences = np.array(valSentences)
-testSentences = sentenceEmbedding(testData,testSpamList)
-testSpamList = convertSpamToBinary(testSpamList)
-testSentences = np.array([np.array(i) for i in testSentences])
-# Model
-model = nn.Sequential(
-    nn.Linear(300, 1)
-)
-torch.manual_seed(42)
-loss_fn = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters(), lr = 0.01)
-my_model = StepByStep(model, loss_fn, optimizer)
-trainSentences = torch.as_tensor(trainSentences).float()
-trainingSpamList = torch.as_tensor(trainingSpamList).float()
-valSentences = torch.as_tensor(valSentences).float()
-valSpamList = torch.as_tensor(valSpamList).float()
-testSentences = torch.as_tensor(testSentences).float()
-train_dataset = TensorDataset(trainSentences,trainingSpamList)
-val_dataset = TensorDataset(valSentences,valSpamList)
-train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(dataset=val_dataset, batch_size=32)
-my_model.set_loaders(train_loader, val_loader)
-my_model.train(100)
-fig = my_model.plot_losses()
-plt.show()
-my_model.model.eval()
-out = my_model.model(testSentences)
-print(out)
-prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
-predictions = []
-for i in prob_sigmoid:
-    if i > 0.5:
-        predictions.append(1)
-    else:
-        predictions.append(0)
-correct = 0
-for i in range(len(predictions)):
-    if predictions[i] == testSpamList[i]:
-        correct+=1
-print('accuarcy', correct/len(predictions))
+# trainSentences = sentenceEmbedding(trainingData,trainingSpamList)
+# trainingSpamList = np.array(convertSpamToBinary(trainingSpamList))
+# trainingSpamList = trainingSpamList.reshape(-1,1)
+# trainSentences = np.array(trainSentences)
+# valSentences = sentenceEmbedding(valData,valSpamList)
+# valSpamList = np.array(convertSpamToBinary(valSpamList))
+# valSpamList = valSpamList.reshape(-1,1)
+# valSentences = np.array(valSentences)
+# testSentences = sentenceEmbedding(testData,testSpamList)
+# testSpamList = convertSpamToBinary(testSpamList)
+# testSentences = np.array([np.array(i) for i in testSentences])
+# # Model
+# model = nn.Sequential(
+#     nn.Linear(300, 1)
+# )
+# torch.manual_seed(42)
+# loss_fn = nn.BCEWithLogitsLoss()
+# optimizer = optim.Adam(model.parameters(), lr = 0.01)
+# my_model = StepByStep(model, loss_fn, optimizer)
+# trainSentences = torch.as_tensor(trainSentences).float()
+# trainingSpamList = torch.as_tensor(trainingSpamList).float()
+# valSentences = torch.as_tensor(valSentences).float()
+# valSpamList = torch.as_tensor(valSpamList).float()
+# testSentences = torch.as_tensor(testSentences).float()
+# train_dataset = TensorDataset(trainSentences,trainingSpamList)
+# val_dataset = TensorDataset(valSentences,valSpamList)
+# train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
+# val_loader = DataLoader(dataset=val_dataset, batch_size=32)
+# my_model.set_loaders(train_loader, val_loader)
+# my_model.train(100)
+# fig = my_model.plot_losses()
+# plt.show()
+# my_model.model.eval()
+# out = my_model.model(testSentences)
+# print(out)
+# prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
+# predictions = []
+# for i in prob_sigmoid:
+#     if i > 0.5:
+#         predictions.append(1)
+#     else:
+#         predictions.append(0)
+# correct = 0
+# for i in range(len(predictions)):
+#     if predictions[i] == testSpamList[i]:
+#         correct+=1
+# print('accuarcy', correct/len(predictions))
 
