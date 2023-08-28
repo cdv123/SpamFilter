ec37575ee39ffaa57c8840d9cabb2912b716f308
cdv123 <charles.duboisveltman@gmail.com>
2023-08-28 15:51:41 +0100

    Removed useless files

--- naivesBayesMethod.py
+++ naivesBayesMethod.py
@@ -1,71 +1,59 @@
-from simplifyDataset import loadTrainingData
-from simplifyDataset import loadTestData
-from simplifyDataset import loadSMS
 from collections import Counter
-import numpy as np
-import csv
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-trainingData,spamList = loadSMS('SMSSpamCollection.csv')
-mostCommonWords = []
-allTrainingWords = []
-spamWordsSet = set()
-spamWordsList = []
-hamWordsSet = set()
-hamWordsList = []
-spamCount = 0
-length = len(trainingData)
-hamCount = 0
-correctCount =0
-for row in range(length):
-    rowWords = trainingData[row].split(" ")
-    if spamList[row] == "spam":
-        for i in rowWords:
-            if i !='':
-                spamCount+=1
-                spamWordsList.append(i)
-    else:
-        for i in rowWords:
-            if i !='':
-                hamCount+=1
-                hamWordsList.append(i)
-allTrainingWords = hamWordsList+spamWordsList
-vocab = set(allTrainingWords)
-word_count = Counter(allTrainingWords)
-word_count = word_count.most_common(1000)
-mostCommonWords = []
-for i in range(0,len(word_count)):
-    mostCommonWords.append(word_count[i][0])
-wordProbSpam = {}
-wordProbHam = {}
-for i in mostCommonWords:
-    wordProbSpam[i] = (spamWordsList.count(i))/(spamCount+len(vocab))
-    wordProbHam[i] = (hamWordsList.count(i))/(hamCount+len(vocab))
-print({k: v for k, v in sorted(wordProbSpam.items(), key=lambda item: item[1])})
-print({k: v for k, v in sorted(wordProbHam.items(), key=lambda item: item[1])})
-incorrectCount = 0
-isSpam = True
-testData,spamTestList = loadSMS('SMSTest.csv')
-for i in range(len(testData)):
-    hamProb = 0.85
-    spamProb = 0.15
-    row = testData[i].split(" ")
-    for j in row:
-        if j != '':
-            if wordProbHam.get(j) == None or wordProbHam.get(j) == 0:  
-                wordProbHam[j] = 1/2
-                wordProbSpam[j] = 1/2
-            hamProb*=wordProbHam[j]
-            spamProb*= wordProbSpam[j]
-    if hamProb > spamProb:
-        isSpam = False
-    else:
-        isSpam = True
-    if (isSpam == True and spamTestList[i] == "spam") or (isSpam == False and spamTestList[i] == "ham"):
-        correctCount+=1
-    else:
-        incorrectCount+=1
-accuracy = correctCount/(incorrectCount+correctCount)
-print(accuracy)
+def trainModel(trainingData,spamList):    
+    mostCommonWords = []
+    allTrainingWords = []
+    spamWordsList = []
+    hamWordsList = []
+    spamCount = 0
+    length = len(trainingData)
+    hamCount = 0
+    for row in range(length):
+        rowWords = trainingData[row].split(" ")
+        if spamList[row] == 1:
+            for i in rowWords:
+                if i !='':
+                    spamCount+=1
+                    spamWordsList.append(i)
+        else:
+            for i in rowWords:
+                if i !='':
+                    hamCount+=1
+                    hamWordsList.append(i)
+    allTrainingWords = hamWordsList+spamWordsList
+    vocab = set(allTrainingWords)
+    word_count = Counter(allTrainingWords)
+    word_count = word_count.most_common(1000)
+    mostCommonWords = []
+    for i in range(0,len(word_count)):
+        mostCommonWords.append(word_count[i][0])
+    wordProbSpam = {}
+    wordProbHam = {}
+    for i in mostCommonWords:
+        wordProbSpam[i] = (spamWordsList.count(i))/(spamCount+len(vocab))
+        wordProbHam[i] = (hamWordsList.count(i))/(hamCount+len(vocab))
+    return wordProbHam,wordProbSpam
+def useModel(testData,spamTestList,wordProbHam,wordProbSpam):
+    correctCount =0
+    incorrectCount = 0
+    isSpam = True
+    for i in range(len(testData)):
+        hamProb = 0.85
+        spamProb = 0.15
+        row = testData[i].split(" ")
+        for j in row:
+            if j != '':
+                if wordProbHam.get(j) == None or wordProbHam.get(j) == 0:  
+                    wordProbHam[j] = 1/2
+                    wordProbSpam[j] = 1/2
+                hamProb*=wordProbHam[j]
+                spamProb*= wordProbSpam[j]
+        if hamProb > spamProb:
+            isSpam = False
+        else:
+            isSpam = True
+        if (isSpam == True and spamTestList[i] == 1) or (isSpam == False and spamTestList[i] == 0):
+            correctCount+=1
+        else:
+            incorrectCount+=1
+    accuracy = correctCount/(incorrectCount+correctCount)
+    return accuracy
--- oneHot.py
+++ oneHot.py
@@ -1,13 +1,4 @@
-
-
-#stepbystep framework used (but simpler version used, from https://github.com/dvgodoy/PyTorchStepByStep/)
 from collections import Counter
-
-
-
-#This code used https://datajenius.com/2022/03/13/a-deep-dive-into-nlp-tokenization-encoding-word-embeddings-sentence-embeddings-word2vec-bert/ as a resource, but different application
-
-# First implementation with One-Hot encoding
 
 def getMostCommonWords(dataset,tokenNum):
     allWords = []
@@ -32,61 +23,3 @@ def oneHotEncode(dataset,mostCommonWords):
             if mostCommonWords[j] in row:
                 oneHot[i][j] = 1
     return oneHot
-# trainingData,spamListTrain = loadSMS('SMSSpamcollection.csv')
-# valData,spamListVal = loadSMS('SMSVal.csv')
-# testData,spamListTest = loadSMS('SMSTest.csv')
-# spamListTrain = np.array(convertSpamToBinary(spamListTrain))
-# spamListTrain = spamListTrain.reshape(-1,1)
-# spamListVal = np.array(convertSpamToBinary(spamListVal))
-# spamListVal = spamListVal.reshape(-1,1)
-# spamListTest = convertSpamToBinary(spamListTest)
-# # spamListTest2 = np.array(spamListTest)
-# # spamListTest.reshape(-1,1)
-# mostCommonWords = getMostCommonWords(trainingData,300)
-# oneHotTrain = oneHotEncode(trainingData,mostCommonWords).values()
-# oneHotTrain = np.array([np.array(i) for i in oneHotTrain])
-# oneHotVal = oneHotEncode(valData,mostCommonWords).values()
-# oneHotVal = np.array([np.array(i) for i in oneHotVal])
-# oneHotTest = oneHotEncode(testData,mostCommonWords).values()
-# oneHotTest = np.array([np.array(i) for i in oneHotTest])
-
-
-# # Model
-# model = nn.Sequential(
-#     nn.Linear(300, 1)
-# )
-# loss_fn = nn.BCEWithLogitsLoss()
-# optimizer = optim.Adam(model.parameters(), lr = 0.01)
-# my_model = StepByStep(model, loss_fn, optimizer)
-# oneHotTrain = torch.as_tensor(oneHotTrain).float()
-# spamListTrain = torch.as_tensor(spamListTrain).float()
-# oneHotVal = torch.as_tensor(oneHotVal).float()
-# spamListVal = torch.as_tensor(spamListVal).float()
-# oneHotTest = torch.as_tensor(oneHotTest).float()
-# # spamListTest = torch.as_tensor(oneHotTest).float()
-# train_dataset = TensorDataset(oneHotTrain,spamListTrain)
-# val_dataset = TensorDataset(oneHotVal,spamListVal)
-# # test_dataset = TensorDataset(oneHotTest,spamListTest)
-# train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-# val_loader = DataLoader(dataset=val_dataset, batch_size=32)
-# print(train_loader)
-# my_model.set_loaders(train_loader, val_loader)
-# my_model.train(100)
-# fig = my_model.plot_losses()
-# plt.show()
-# my_model.model.eval()
-# out = my_model.model(oneHotTest)
-# print(out)
-# prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
-# predictions = []
-# for i in prob_sigmoid:
-#     if i > 0.5:
-#         predictions.append(1)
-#     else:
-#         predictions.append(0)
-# correct = 0
-# for i in range(len(predictions)):
-#     if predictions[i] == spamListTest[i]:
-#         correct+=1
-
-# print('accuarcy', correct/len(predictions))
