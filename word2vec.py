4ea5c166afebbf8ec38297cbb13617f3402704cf
cdv123 <charles.duboisveltman@gmail.com>
2023-08-28 16:30:13 +0100

    Removed more useless files and read dataset using url

--- word2vec.py
+++ word2vec.py
@@ -1,18 +1,4 @@
 import numpy as np
-#stepbystep framework used (but simpler version used, from https://github.com/dvgodoy/PyTorchStepByStep/)
-
-
-#This code used https://datajenius.com/2022/03/13/a-deep-dive-into-nlp-tokenization-encoding-word-embeddings-sentence-embeddings-word2vec-bert/ as a resource, but different application
-
-#Download vocab
-
-# word2vec = downloader.load('word2vec-google-news-300')
-
-# trainingData, trainingSpamList = loadSMS('SMSSpamCollection.csv')
-# valData, valSpamList = loadSMS('SMSVal.csv')
-# testData, testSpamList = loadSMS('SMSTest.csv')
-
-#average out all word embeddings into sentence embedding
 def sentenceEmbedding(data,spamList,word2vec,vector_size):
     sentenceEmbeddings = []
     dataLength = len(data)
@@ -39,52 +25,41 @@ def sentenceEmbedding(data,spamList,word2vec,vector_size):
             sentenceEmbeddings.append(sentence)
         i+=1
     return sentenceEmbeddings
+def tokenize(trainingData):
+    wordEmbedding = []
+    for i in range(len(trainingData)):
+        row = trainingData[i].split(" ")
+        newRow = []
+        for word in row:
+            if word != '':
+                newRow.append(word)
+        if newRow != []:
+            wordEmbedding.append(newRow)
+    return wordEmbedding
 
-# trainSentences = sentenceEmbedding(trainingData,trainingSpamList)
-# trainingSpamList = np.array(convertSpamToBinary(trainingSpamList))
-# trainingSpamList = trainingSpamList.reshape(-1,1)
-# trainSentences = np.array(trainSentences)
-# valSentences = sentenceEmbedding(valData,valSpamList)
-# valSpamList = np.array(convertSpamToBinary(valSpamList))
-# valSpamList = valSpamList.reshape(-1,1)
-# valSentences = np.array(valSentences)
-# testSentences = sentenceEmbedding(testData,testSpamList)
-# testSpamList = convertSpamToBinary(testSpamList)
-# testSentences = np.array([np.array(i) for i in testSentences])
-# # Model
-# model = nn.Sequential(
-#     nn.Linear(300, 1)
-# )
-# torch.manual_seed(42)
-# loss_fn = nn.BCEWithLogitsLoss()
-# optimizer = optim.Adam(model.parameters(), lr = 0.01)
-# my_model = StepByStep(model, loss_fn, optimizer)
-# trainSentences = torch.as_tensor(trainSentences).float()
-# trainingSpamList = torch.as_tensor(trainingSpamList).float()
-# valSentences = torch.as_tensor(valSentences).float()
-# valSpamList = torch.as_tensor(valSpamList).float()
-# testSentences = torch.as_tensor(testSentences).float()
-# train_dataset = TensorDataset(trainSentences,trainingSpamList)
-# val_dataset = TensorDataset(valSentences,valSpamList)
-# train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-# val_loader = DataLoader(dataset=val_dataset, batch_size=32)
-# my_model.set_loaders(train_loader, val_loader)
-# my_model.train(100)
-# fig = my_model.plot_losses()
-# plt.show()
-# my_model.model.eval()
-# out = my_model.model(testSentences)
-# print(out)
-# prob_sigmoid = torch.sigmoid(out).squeeze().tolist()
-# predictions = []
-# for i in prob_sigmoid:
-#     if i > 0.5:
-#         predictions.append(1)
-#     else:
-#         predictions.append(0)
-# correct = 0
-# for i in range(len(predictions)):
-#     if predictions[i] == testSpamList[i]:
-#         correct+=1
-# print('accuarcy', correct/len(predictions))
+def sentenceEmbedding2(data,spamList,word2vec,vector_size):
+    sentenceEmbeddings = []
+    dataLength = len(data)
+    i = 0
+    while i < dataLength:
+        sentence = np.zeros(vector_size)
+        row = data[i].split(" ")
+        length = 0
+        for j in row:
+            try:
+                sentence = np.add(sentence,np.array(word2vec.wv[j]))
+                length+=1
+            except:
+                pass
+        if length ==0:
+            data.pop(i)
+            spamList.pop(i)
+            dataLength-=1
+            i-=1
+        else:
+            for dim in sentence:
+                dim = dim/length
+            sentenceEmbeddings.append(sentence)
+        i+=1
+    return sentenceEmbeddings
 
