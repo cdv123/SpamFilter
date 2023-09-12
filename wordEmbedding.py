97f41d1b0845e554371569b7e5241fc80fa0a35c
cdv123 <115459644+cdv123@users.noreply.github.com>
2023-09-12 17:06:51 +0100

    Skip gram model works, speed needs to be improved

--- wordEmbedding.py
+++ wordEmbedding.py
@@ -1,7 +1,11 @@
 from collections import Counter
-from simplifyDataset import loadSMS2
+from simplifyDataset import loadSMS2, convertSpamToBinary
 import numpy as np
 import random
+from numpyNetwork import neuralNetwork,testNetwork
+from word2vec import sentenceEmbedding
+import cProfile
+
 #Steps
 #Read text
 #Preprocess text
@@ -66,16 +70,20 @@ def make_matrices(word_dict,pairs):
         context_matrix.append(context_word_index)
     return focus_matrix,context_matrix
 
-def skip_gram_model(focus_matrix,context_matrix,dim,epochs,lr,vector_size): 
+def skip_gram_model(focus_matrix,context_matrix,dim,epochs,lr,text,word_dict,n_words): 
     np.random.seed(0)
+    vector_size = len(word_dict)
     limit= math.sqrt(6/(vector_size+dim))
+    text_size = len(text)
     # weight matrix to go from input layer to hidden layer
+    # hidden_w = np.random.uniform(-limit,limit,size=(vector_size,dim))
     hidden_w = np.random.uniform(-limit,limit,size=(vector_size,dim))
     # hiddenW = np.zeros((vectorSize,hiddenNodesNum))
     limit=math.sqrt(6/(1+dim))
     # weight vector to go from hidden layer to output layer
     output_w = np.random.uniform(-limit,limit,size=(dim,vector_size))
     loss = 0
+    text_size = len(text)
     # outputW =np.zeros(hiddenNodesNum)
     for epoch in range(epochs):
         for x in range(len(focus_matrix)):
@@ -85,7 +93,7 @@ def skip_gram_model(focus_matrix,context_matrix,dim,epochs,lr,vector_size):
             final_res = softmax(final_res)
             loss += loss_function(final_res,context_matrix[x])
             error_der = softmax_der(final_res,context_matrix[x])
-            hidden_w,output_w = back_propagation(output_w,hidden_w,focus_matrix[x],hidden_res,error_der,lr)
+            hidden_w,output_w = back_propagation(output_w,hidden_w,focus_matrix[x],hidden_res,error_der,lr,text_size,text,n_words,word_dict)
             # gradx = compute_gradient(hidden_res,error_der)
             # hidden_loss = hidden_w.transpose()*error_der
             # grad_hidden_w = compute_hidden_gradient(hidden_loss,focus_matrix[x]) 
@@ -94,7 +102,7 @@ def skip_gram_model(focus_matrix,context_matrix,dim,epochs,lr,vector_size):
         loss = loss/(len(focus_matrix))
         print(epoch,loss)
         loss = 0
-    return hidden_w,output_w
+    return hidden_w
 def forward_pass(x,hidden_w,output_w):
     # since input vector only has a single 1, the output of the first matrix multiplication 
     # is the row corresponding to the index of this 1
@@ -127,10 +135,16 @@ def update_weights(w,dw,lr):
     w = w-dw*lr
     return w
 
-def back_propagation(output_w,hidden_w,input,hidden_res,error,lr):
-    grad_output_w = np.outer(hidden_res,error)
+def back_propagation(output_w,hidden_w,input,hidden_res,error,lr,text_size,text_words,n_words,word_dict):
+    words_to_change = choose_samples(text_size,text_words,n_words,word_dict)
+    grad_output_w = np.zeros((len(words_to_change),len(hidden_res)))
+    for i in range(len(words_to_change)):
+        grad_output_w[i] = error[words_to_change[i]]*hidden_res
+    # grad_output_w = np.outer(hidden_res,error)
+    # print(np.shape(error), np.shape(hidden_res))
     grad_hidden_w = np.dot(output_w,error.T)
-    output_w = output_w - lr*grad_output_w
+    for i in range(len(words_to_change)):
+        output_w[:,words_to_change[i]] = output_w[:,words_to_change[i]] - grad_output_w[i]*lr
     hidden_w[input] = hidden_w[input]-lr*grad_hidden_w
     return hidden_w,output_w
 def subsampling(prob):
@@ -141,7 +155,14 @@ def subsampling(prob):
 
 def negative_sampling_prob():
     pass
-training_data,spam_data = loadSMS2("SMSValidation.txt")
+
+def choose_samples(text_size,text_words,n_words,word_dict):
+    samples = []
+    for i in range(n_words):
+        rnd_num = random.randint(0,text_size-1)
+        samples.append(word_dict[text_words[rnd_num]])
+    return samples
+training_data,spam_data = loadSMS2("SMSVal.txt")
 text = get_text(training_data)
 text_num = len(text)
 text_set = set(text)
@@ -150,10 +171,27 @@ new_text = set()
 for i in text_set:  
     if subsampling(sampling_rate(text_counter[i]/text_num)): 
         new_text.add(i)
-pairs = make_all_pairs(training_data,5,text)
+pairs = make_all_pairs(training_data,2,text)
+text_l = list(new_text)
 unique_word_dict = uniqueWordDict(text)
+print(unique_word_dict)
 focus_matrix,context_matrix = make_matrices(unique_word_dict,pairs)
-skip_gram_model(focus_matrix,context_matrix,50,10,0.01,len(unique_word_dict))
-# print(len(new_text))  
-# text_counter = text_counter.most_common()
-# print(text_counter)
+# hidden_w = skip_gram_model(focus_matrix,context_matrix,50,6,0.3,list(new_text),unique_word_dict,20)
+cProfile.run('skip_gram_model(focus_matrix,context_matrix,50,6,0.3,list(new_text),unique_word_dict,20)')
+# word_embedding = {}
+# for i in unique_word_dict:
+#     word_embedding[i] = hidden_w[unique_word_dict[i]]
+# # training_data2,spam_data2 = loadSMS2('SMSSpamCollection.txt')
+# spam_data = convertSpamToBinary(spam_data)
+# vector_size = 50    
+# trainSentences = sentenceEmbedding(training_data,spam_data,word_embedding,50)
+# # mostCommonWords = getMostCommonWords(trainingData,vector_size)
+# # oneHotTrain = list(oneHotEncode(trainingData,mostCommonWords).values())
+# # weights,bias,hiddenW,hiddenBias = neuralNetwork(trainSentences,spam_data,0.004,9)
+# weights,bias = neuralNetwork(trainSentences,spam_data,0.003,20)
+# # print(bias)
+# testData, spamTest = loadSMS2('SMSTest.txt')
+# # oneHotTest = list(oneHotEncode(testData,mostCommonWords).values())
+# spamTest = convertSpamToBinary(spamTest)
+# testSentences = sentenceEmbedding(testData,spamTest,word_embedding,50)
+# print(testNetwork(testSentences,spamTest,weights,bias))
