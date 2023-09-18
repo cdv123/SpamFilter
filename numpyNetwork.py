0f7ff2aa18fef34d2c051a2028d4cb9f4ff12a2d
cdv123 <115459644+cdv123@users.noreply.github.com>
2023-09-18 12:01:21 +0100

    Added mini-batch

--- numpyNetwork.py
+++ numpyNetwork.py
@@ -39,9 +39,56 @@ def neuralNetwork(trainingData,spamData,lr,epochs,val_data,val_spam):
         error[epoch] = error[epoch]/len(trainingData)
         error_val[epoch] = error_val[epoch]/len(trainingData)
     return w,b,error,error_val
-
+def neuralNetwork2(mini_batches,val_mini_batches,lr,epochs):
+    np.random.seed(0)
+    random.seed(0)
+    vectorSize = len(mini_batches[0][0][0])
+    #initialise weights
+    w = np.random.rand(vectorSize)
+    #initialise m and v for adam optimizer
+    m = np.zeros(vectorSize)
+    v = np.zeros(vectorSize)
+    #initialise bias
+    b = random.random()
+    #train step - 
+    error = [0]*epochs
+    error_val = [0]*epochs
+    for epoch in range(epochs):
+        for x in range(len(mini_batches)):
+            y = x
+            while y >= len(val_mini_batches):
+                y-= len(val_mini_batches)
+            gradw = np.zeros(vectorSize)
+            gradb = 0
+            for i in range(len(mini_batches[x])):
+                j = i
+                while j >= len(val_mini_batches[y]):
+                    j-=len(val_mini_batches[y])
+                # Compute prediction
+                a = getOutput(mini_batches[x][i][0],w,b)
+                a = sigmoid(a)
+                a_val = getOutput(val_mini_batches[y][j][0],w,b)
+                a_val = sigmoid(a_val)
+                # activation function - normalise value
+                error[epoch]+= BCE(mini_batches[x][i][1],a) 
+                error_val[epoch]+= BCE(val_mini_batches[y][j][1],a_val)
+                # compute gradient loss (actual loss not needed)
+                new_gradw,new_gradb = computeGradient(mini_batches[x][i][0],mini_batches[x][i][1],a)
+                gradb+=new_gradb
+                gradw+=new_gradw
+                # update weights using gradient loss
+            gradb = gradb/len(mini_batches[x])
+            gradw = gradw/len(mini_batches[x])
+            w,b = updateWeights(w,b,gradw,gradb,lr)
+        error[epoch] = error[epoch]/len(trainingData)
+        error_val[epoch] = error_val[epoch]/len(trainingData)
+    return w,b,error,error_val
 def sigmoid(x):
     return (1/(1+(np.exp(-x))))
+
+def getBatchOutput(mini_batch,w,b):
+    a = [sigmoid(getOutput(x[0],w,b)) for x in mini_batch]
+    return a
 
 def sigmoidDerivative(yhat):
     return (yhat)*(1-yhat)
@@ -51,6 +98,10 @@ def BCE(y,logit):
     loss = np.multiply(-y,np.log(logit))-np.multiply((1-y),np.log(1-logit))
     return loss
 
+def BCEbatch(mini_batch,a):
+    err_list = [BCE(mini_batch[x][1],a[x]) for x in range(len(mini_batch))]
+    avg_err = np.sum(err_list)*1/len(mini_batch)
+    return avg_err
 def getOutput(x,w,b):
     return (np.matmul(x,w)+b)
 
@@ -61,6 +112,12 @@ def BCEgrad(y,yhat):
 def computeGradient(x,y,a):
     gradb = BCEgrad(y,a)*sigmoidDerivative(a)
     gradx = np.multiply(gradb,x)
+    return gradx,gradb
+
+def computeBatchGradient(mini_batch,a):
+    gradArr = np.array([computeGradient(mini_batch[x][0],mini_batch[x][1],a[x]) for x in range(len(mini_batch))])
+    gradx = np.sum(gradArr[:,0])/len(mini_batch)
+    gradb = np.sum(gradArr[:,1])/len(mini_batch)
     return gradx,gradb
 def updateWeights(w,b,dw,db,lr):
     w = w-lr*dw
@@ -94,6 +151,18 @@ def testNetwork(testData,spamTest,w,b):
             incorrect+=1
     accuarcy = correct/(correct+incorrect)
     return accuarcy
+
+def make_mini_batches(trainingData,spamData,batch_size):
+    mini_batches = []
+    i = 0
+    while i < len(trainingData):
+        mini_batches.append([])
+        for j in range(batch_size):
+            if i >= len(trainingData):
+                break
+            mini_batches[-1].append((trainingData[i],spamData[i]))
+            i+=1
+    return mini_batches
 
 def twoLayerNetwork(trainingData,spamData,lr,hiddenNodesNum,epochs,val_data,val_spam):
     vectorSize = len(trainingData[0])
@@ -188,34 +257,37 @@ def testTwoLayer(testData,spamData,hiddenW,hiddenBias,outputW,outputBias):
         if prediction == spamData[i]:
             correctCount+=1
     return correctCount/len(testData)
-# trainingData,spamData = loadSMS2('SMSSpamCollection.txt')
-# spamData = convertSpamToBinary(spamData)
-# valData,spamVal = loadSMS2('SMSVal.txt')
-# spamVal = convertSpamToBinary(spamVal)
-# vector_size = 100
-# embeddingDict = useEmbedding2()
-# trainSentences = sentenceEmbedding(trainingData,spamData,embeddingDict,100)
-# valSentences = sentenceEmbedding(valData,spamVal,embeddingDict,100)
+trainingData,spamData = loadSMS2('SMSSpamCollection.txt')
+spamData = convertSpamToBinary(spamData)
+valData,spamVal = loadSMS2('SMSVal.txt')
+spamVal = convertSpamToBinary(spamVal)
+vector_size = 100
+embeddingDict = useEmbedding2()
+trainSentences = sentenceEmbedding(trainingData,spamData,embeddingDict,100)
+valSentences = sentenceEmbedding(valData,spamVal,embeddingDict,100)
+mini_batches = make_mini_batches(trainSentences,spamData,10)
+val_batches = make_mini_batches(valSentences,spamVal,10)
 # # mostCommonWords = getMostCommonWords(trainingData,vector_size)
 # # oneHotTrain = list(oneHotEncode(trainingData,mostCommonWords).values())
 # # oneHotVal = list(oneHotEncode(valData,mostCommonWords).values())
 # # hiddenW,hiddenBias,weights,bias,error,error_val = twoLayerNetwork(oneHotTrain,spamData,0.00025,9,20,oneHotVal,spamVal)
 # # hiddenW,hiddenBias,weights,bias,error,error_val = twoLayerNetwork(oneHotTrain,spamData,0.004,9,10,oneHotVal,spamVal)
 # hiddenW,hiddenBias,weights,bias,error,error_val = twoLayerNetwork(trainSentences,spamData,0.004,9,10,valSentences,spamVal)
-# # weights,bias,error,error_val = neuralNetwork(trainSentences,spamData,0.001,20,valSentences,spamVal)
+# weights,bias,error,error_val = neuralNetwork2(mini_batches,val_batches,0.008,20)
+weights,bias,error,error_val = neuralNetwork(trainSentences,spamData,0.003,20,valSentences,spamVal)
 # # weights,bias,error,error_val = neuralNetwork(oneHotTrain,spamData,0.03,10,oneHotVal,spamVal)
 # # # print(bias)
-# plt.plot(range(10),error, label = "Training Loss")
-# plt.plot(range(10),error_val, label = "Validation Loss")
-# plt.legend()
-# plt.show()
-# testData, spamTest = loadSMS2('SMSTest.txt')
+plt.plot(range(20),error, label = "Training Loss")
+plt.plot(range(20),error_val, label = "Validation Loss")
+plt.legend()
+plt.show()
+testData, spamTest = loadSMS2('SMSTest.txt')
 # # oneHotTest = list(oneHotEncode(testData,mostCommonWords).values())
-# spamTest = convertSpamToBinary(spamTest)
-# testSentences = sentenceEmbedding(testData,spamTest,embeddingDict,100)
+spamTest = convertSpamToBinary(spamTest)
+testSentences = sentenceEmbedding(testData,spamTest,embeddingDict,100)
 # print(testTwoLayer(testSentences,spamTest,hiddenW,hiddenBias,weights,bias))
 # print(testTwoLayer(oneHotTest,spamTest,hiddenW,hiddenBias,weights,bias))
-# print(testNetwork(testSentences,spamTest,weights,bias))
+print(testNetwork(testSentences,spamTest,weights,bias))
 # print(testNetwork(oneHotTest,spamTest,weights,bias))
 
 
